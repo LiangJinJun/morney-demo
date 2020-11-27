ea42fc50e3b7b953b1372b3cdf82db7b1839db3c
LiangJinJun <2210258654@qq.com>
2020-11-27 23:19:30 +0800

    使用全局 tagList

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -26,7 +26,6 @@
 
 
     const recordList= recordListModel.fetch();
-    const tagList = tagListModel.fetch();
 
 
 
@@ -34,7 +33,7 @@
       components: {Tags,FormItem, Types, NumberPad},
     })
    export default class Money extends Vue{
-          tags=tagList;
+          tags=window.tagList;
           recordList: RecordItem[]=recordList;
           record: RecordItem={
             tags:[],   /*初始值*/
