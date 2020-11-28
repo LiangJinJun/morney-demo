5dfe1983a5dd253d7c74f0581c510a43ff9f902a
LiangJinJun <2210258654@qq.com>
2020-11-28 21:45:37 +0800

    封装 record 的操作

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
 import Icon from '@/components/Icon.vue';
 import tagListModel from '@/models/tagListModel';
+import recordListModel from '@/models/recordListModel';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.component('Nav',Nav);
-Vue.component('Layout',Layout);
+Vue.component('Nav', Nav);
+Vue.component('Layout', Layout);
 
-Vue.component('Icon',Icon);
+Vue.component('Icon', Icon);
+
+//record store
+window.recordList = recordListModel.fetch();
+window.createRecord = (record: RecordItem) => recordListModel.create(record);
+//tag store
 
 window.tagList = tagListModel.fetch();
-window.findTag=(id: string)=> {
-  return window.tagList.filter(t=>t.id===id)[0];
+window.findTag = (id: string) => {
+  return window.tagList.filter(t => t.id === id)[0];
 };
-window.createTag=(name: string)=>{
-  const message=tagListModel.create(name)
-    if (message==='duplicated'){
-      window.alert('标签名重复了')
-    }else if (message==='success'){
+window.createTag = (name: string) => {
+  const message = tagListModel.create(name);
+  if (message === 'duplicated') {
+    window.alert('标签名重复了');
+  } else if (message === 'success') {
       window.alert('添加成功');
   }
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,11 +20,9 @@
    import Types from '@/components/Money/Types.vue';
    import FormItem from '@/components/Money/FormItem.vue';
    import Tags from '@/components/Money/Tags.vue';
-    import {Component, Watch} from 'vue-property-decorator';
-    import recordListModel from '@/models/recordListModel';
+    import {Component} from 'vue-property-decorator';
 
 
-    const recordList= recordListModel.fetch();
 
 
 
@@ -33,7 +31,7 @@
     })
    export default class Money extends Vue{
           tags=window.tagList;
-          recordList: RecordItem[]=recordList;
+          recordList=window.recordList;
           record: RecordItem={
             tags:[],   /*初始值*/
             notes:'',
@@ -45,12 +43,9 @@
           }
 
           saveRecord(){
-            recordListModel.create(this.record)
+            window.createRecord(this.record)
           }
-          @Watch('recordList')
-          onRecordListChanged(){
-            recordListModel.save();
-          }
+
    }
 </script>
 
