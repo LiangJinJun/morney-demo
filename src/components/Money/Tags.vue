f10db9af247384d343cc9f45e4c6e8ede1d1fe1a
LiangJinJun <2210258654@qq.com>
2020-12-03 20:25:16 +0800

    Revert "Labels.vue 使用 Vuex,抽离TagHelper作为mixin"
    This reverts commit 98898085

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="tags">
       <div class="new">
-         <button @click="createTag">新增标签</button>
+         <button @click="create">新增标签</button>
       </div>
       <ul class="current">
          <li v-for="tag in tagList" :key="tag.id" @click="toggle(tag)" :class="{selected: selectedTags.indexOf(tag)>=0}">{{tag.name}}</li>
@@ -13,8 +13,6 @@
 <script lang="ts">
    import Vue from 'vue';
    import {Component, Prop} from 'vue-property-decorator';
-   import {mixins} from 'vue-class-component';
-   import TagHelper from '@/mixins/TagHelper';
    @Component({
       computed:{
          tagList(){
@@ -22,7 +20,7 @@
          }
       }
    })
-  export  default  class Tags extends mixins(TagHelper) {
+  export  default  class Tags extends Vue {
       selectedTags: string[]=[];
 
       created() {
@@ -37,6 +35,11 @@
             this.selectedTags.push(tag)
          }
          this.$emit('update:value',this.selectedTags)
+      }
+      create(){
+        const name=window.prompt('请输入标签名');
+       if(!name){return window.alert('标签名不能为空');}
+          this.$store.commit('createTag',name);
       }
 
   }
