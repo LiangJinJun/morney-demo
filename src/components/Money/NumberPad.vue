c9bc8136b7045d565d49603add64f8cadb969fe8
LiangJinJun <2210258654@qq.com>
2020-12-06 03:30:27 +0800

    完成统计页面

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -26,7 +26,7 @@
 
    @Component
    export default class NumberPad extends Vue{
-     @Prop() readonly value!: number;
+     @Prop(Number) readonly value!: number;
      output=this.value.toString();
          inputContent(event: MouseEvent){
             const button = (event.target as HTMLButtonElement);
@@ -48,7 +48,6 @@
             this.output+=input
          }
       remove(){
-          const x=this.output.slice(0,-1)
          if(this.output.length===1){
             this.output='0'
          }else {
@@ -60,8 +59,9 @@
             this.output='0'
       }
       ok(){
-            this.$emit('update:value',this.output);
-            this.$emit('submit',this.output);
+            const number = parseFloat(this.output)
+            this.$emit('update:value',number);
+            this.$emit('submit',number);
             this.output='0';
       }
 
