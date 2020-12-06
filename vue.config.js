c79bcbd039ee9945745165811b8f358c355fd2c9
LiangJinJun <2210258654@qq.com>
2020-12-06 17:01:07 +0800

    修改vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/YiXin-website/" : "/",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/morney-demo-website /'
+    : '/',
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, "src/assets/icons")
