1cf8f0f751fd61ef7af5a2235cab113e41afd2fe
LiangJinJun <2210258654@qq.com>
2020-12-06 16:25:28 +0800

    部署网站到 morney-demo-website

--- deploy.sh
+++ deploy.sh
@@ -22,4 +22,6 @@ git commit -m 'deploy'
 # 部署到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:LiangJinJun/morney-demo-website.git master:gh-pages
 
+git push -f git@gitee.com:liangjinjun/morney-demo-website.git master:gh-pages
+
 cd -
